a92ee2618779411450a11920c4ee9cc2bfb64f76
jingtao <2205674160@qq.com>
2025-09-30 13:41:11 +0800

    add

--- src/main/java/qiuqiu/WxCloudRunApplication.java
+++ src/main/java/qiuqiu/WxCloudRunApplication.java
@@ -1,14 +1,17 @@
 package qiuqiu;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan(basePackages = {"qiuqiu.dao"})
-public class WxCloudRunApplication {  
+@Slf4j
+public class WxCloudRunApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(WxCloudRunApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(WxCloudRunApplication.class, args);
+        log.info("start success");
+    }
 }
