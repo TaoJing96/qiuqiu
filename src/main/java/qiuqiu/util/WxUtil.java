3bf26d8714d8fe6f3eb9b1f2dd9ff49ba7655113
jingtao <2205674160@qq.com>
2025-10-09 17:54:41 +0800

    add

--- src/main/java/qiuqiu/util/WxUtil.java
+++ src/main/java/qiuqiu/util/WxUtil.java
@@ -4,11 +4,6 @@ import com.qcloud.cos.COSClient;
 import com.qcloud.cos.model.ObjectMetadata;
 import com.qcloud.cos.model.PutObjectRequest;
 import com.qcloud.cos.model.PutObjectResult;
-import com.squareup.okhttp.OkHttpClient;
-import me.chanjar.weixin.mp.api.WxMpService;
-import me.chanjar.weixin.mp.api.impl.WxMpServiceImpl;
-import me.chanjar.weixin.mp.config.WxMpHostConfig;
-import me.chanjar.weixin.mp.config.impl.WxMpDefaultConfigImpl;
 
 import java.io.InputStream;
 
@@ -20,21 +15,8 @@ import static com.qcloud.cos.demo.ci.ClientUtils.getCosClient;
  */
 public class WxUtil {
 
-    private static final WxMpService WX_MP_SERVICE = initWxMpService();
-    private static final OkHttpClient httpClient = new OkHttpClient();
     private static final String BUCKET_NAME = "7072-prod-7go6fjep656bf9a5-1316521764";
     private static final String REGION = "ap-shanghai";
-
-    private static WxMpService initWxMpService() {
-        WxMpDefaultConfigImpl config = new WxMpDefaultConfigImpl();
-        config.setAppId("wx505e6119afdd7117");
-        config.setSecret("AAQ9G7sEAAABAAAAAAAAvxLci9Ek8HjaVl/naCAAAAAraAdzKm8i8JwFJ68cDOJBtIHsmv3F8e00LCv7f+tYvtD/TFAZoZAu4s/DMZ9Pl/zk0GdGYDOdrh6ks0Jc1u0jN3WocpvdX5igh0wC3Rse4WMYqJ3NdBF5yai+grGA5o60UQTfNYYB3vkiULYa69j4abES02oi/4I=");
-        config.setHostConfig(new WxMpHostConfig());
-
-        WxMpService wxMpService = new WxMpServiceImpl();
-        wxMpService.setWxMpConfigStorage(config);
-        return wxMpService;
-    }
 
     public static String uploadInputStream(InputStream inputStream, String cosKey, String contentType) {
         COSClient cosClient = null;
